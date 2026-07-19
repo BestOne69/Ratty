6aa48151ff952513b428d0e1c2988593a98acaeb
BestOne69 <faltuhaiye121@gmail.com>
2026-07-19 16:42:38 +0000

    generate password done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import random
+import string
 import streamlit as st
 
 st.title("Ratty")
@@ -7,6 +8,11 @@ st.write(" ")
 
 st.header("Check Your Password Strength")
 
+stage = "main"
+
+if "stage" not in st.session_state:
+    st.session_state.stage = "main"
+
 
 password= st.text_input("Enter password", type="password")
 
@@ -14,66 +20,147 @@ special = "?!()&#@<>^%[]"
 
 score = 0
 
+def main():
+    if st.button("Check Entropy"):
+        st.session_state.stage = "entropy"
+
+    st.write(" ")
+
+    if st.button("Time to Crack"):
+         st.session_state.stage = "crack"
+
+    st.write(" ")
+
+    if st.button("Generate Password"):
+         st.session_state.stage = "generate"
+
 def length():
     if len(password) >= 8:
-        st.succes("Length is Good")
-        score += 1
+        st.success("Length is Good")
+        return 1
 
     else:
         st.error("Minimum 8 digits Allowed....")
+        return 0
 
 def upper():
-    for letter in password :
-
-        if letter.isupper():
+    
+    if any(letter.isupper() for letter in password):
             st.success("Uppercase letter ")
 
-            score += 1
+            return 1 
 
-        else:
+    else:
             st.error("Atleast one Uppercase Required !!")
+            return 0
 
 
 def lower():
     st.write(" ")
 
-    for letter in password:
-        if letter.islower():
+    if any(letter.islower() for letter in password):
 
             st.success("Lowercase Letter available")
-            score += 1
+            return 1
 
-        else:
+        
+        
+    else:
             st.error("Min 1 lowercase letter Required")
+            return 0
 
 def number():
     st.write(" ")
 
-    for letter in password:
-        if letter.isdigit():
+    if any(letter.isdigit() for letter in password):
             st.success("Number is in Password")
 
-            score += 1
+            return 1
 
 
-        else:
+    else:
             st.error("Number in password Required")
+            return 0
 
 
-def special():
+def specials():
     st.write(" ")
 
-    for letter in password:
-        if letter in special:
+    if any(letter in special for letter in special):
 
-            st.success("check")
+            st.success("Special numerics Is in Password")
 
-            score +=1
-        else:
+            return 1
+    else:
             st.error("Special Characters Required")
+
+            return 0
 
 
 def generate():
-    pass 
+    char = string.ascii_letters + string.digits + "!?%@^"
 
-def crack
+    passw = " ".join(random.choice(char)  for _ in range(10))
+    st.write(" ")
+
+    st.code(passw)
+
+    if st.button("New One", type="primary"):
+         st.session_state.stage = "generate"
+
+    st.write(" ")
+
+    if st.button("return to home"):
+         st.session_state.stage = "main"
+
+def crack():
+    if score <= 2:
+        st.write("It can Easily Crack in Few minutes")
+
+    elif score == 3:
+        st.write("Can Crack Few Hours")
+
+    elif score >= 4:
+        st.write("Its Good one maybe Takes Days and years To Crack It")
+
+
+def entropy():
+    st.write(" ")
+
+    ent = len(password) * 4
+
+    st.metric("Entropy Score", ent)
+
+    if st.button(" Return To home"):
+        st.session_state.stage = "main"
+
+
+score += int(upper())
+score += int(lower())
+
+score += int(specials())
+score += int(number())
+score += int(length())
+
+#if st.session_state.stage == "main":
+   # length()
+    #lower()
+  #  upper()
+   # number()
+   # specials()
+if st.session_state.stage == "main":
+     main()
+
+elif st.session_state.stage == "entropy":
+    entropy()
+
+elif st.session_state.stage == "crack":
+     crack()
+
+
+elif st.session_state.stage == "generate":
+     generate()
+
+
+
+
